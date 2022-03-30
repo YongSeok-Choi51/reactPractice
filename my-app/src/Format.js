d8ccd41b759a5540b9a3ff17ad7ebd32263cb605
YongSeok-Choi51 <dydtjr1717@pioncorp.com>
2022-03-30 12:37:31 +0900

    개발자자B init page

---
+++ my-app/src/Format.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Format = () => {
+    return (
+        <table>
+            <th>
+                Name
+            </th>
+            <th>
+                E-mail
+            </th>
+
+            <td>
+                1
+            </td>
+            <td>
+                2
+            </td>
+        </table>
+    )
+}
+
+export default Format
